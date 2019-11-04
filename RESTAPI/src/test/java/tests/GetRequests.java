88e46348cda1c17a3b9ec118283076d4b4d886c5
apunnuri <apunnuri@den01cpm.us.oracle.com>
2019-11-04 06:23:45 -0700

    Added POST content test cases. Modified the getContent test case and added response assertions

--- RESTAPI/src/test/java/tests/GetRequests.java
+++ RESTAPI/src/test/java/tests/GetRequests.java
@@ -7,12 +7,19 @@ import base.Base;
 import base.Endpoints;
 import base.MethodType;
 import io.restassured.http.Header;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import utilities.RestUtilities;
 import static org.hamcrest.Matchers.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class GetRequests {
 	RequestSpecification reqspec;
+	ResponseSpecification resSpec;
 	@BeforeTest
 	public void setup()
 	{
@@ -20,16 +27,21 @@ public class GetRequests {
 		Base.generateAuthToken();
 	}
 	
-	@Test
+	//@Test
 	public void getContent()
 	{
 		RestUtilities.setEndpoint(Endpoints.GETCONTENT);
 		reqspec = RestUtilities.getRequestSpecification();
 		reqspec.header(new Header("kmauthtoken", Base.config.getString("kmauthtoken")));
-		RestUtilities.getResponse(MethodType.GET, null).then().body(containsString("asp"));
+		Response resp = RestUtilities.getResponse(MethodType.GET, null);
+		resp.then().spec(RestUtilities.getResponseSpecification());
+		resp.then().assertThat().header("Access-Control-Allow-Origin", "*");
+//		resp.then().assertThat().statusCode(200);
+		resp.then().assertThat().body(containsStringIgnoringCase("FAQ"));
+		resp.then().assertThat().body("ResultList.count",equalTo("20"));
 	}
 	
-	@Test
+	//@Test
 	public void getContentByID()
 	{
 		RestUtilities.setEndpoint(Endpoints.GETCONTENTBYDOCID);
@@ -38,4 +50,17 @@ public class GetRequests {
 		reqspec.header(new Header("kmauthtoken", Base.config.getString("kmauthtoken")));
 		RestUtilities.getResponse(MethodType.GET, null).then().log().all(true);
 	}
+
+	@Test
+public void postContent()
+{
+	RestUtilities.setEndpoint(Endpoints.POSTCONTENT);
+	reqspec = RestUtilities.getRequestSpecification();
+	reqspec.header(new Header("kmauthtoken", Base.config.getString("kmauthtoken")));
+	try {
+		RestUtilities.getResponse(MethodType.POST, new String(Files.readAllBytes(Paths.get("./src/test/resources/payloads/postContent.xml"))));
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+}
 }
--- RESTAPI/src/test/java/utilities/RestUtilities.java
+++ RESTAPI/src/test/java/utilities/RestUtilities.java
@@ -2,11 +2,13 @@ package utilities;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.configuration.ConfigurationException;
 
 import base.Base;
 import base.MethodType;
+import groovyjarjarantlr.LexerSharedInputState;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.path.json.JsonPath;
@@ -15,6 +17,7 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class RestUtilities {
 
@@ -23,18 +26,15 @@ public class RestUtilities {
 	public static ResponseSpecBuilder RES_SPEC_BUILDER = null;
 	public static ResponseSpecification RES_SPECIFICATION = null;
 	public static String ENDPOINT = null;
-	
-	
-	public static void setEndpoint(String endpoint)
-	{
-		ENDPOINT=endpoint;
-		System.out.println("Endpoint is: "+ENDPOINT);
+
+	public static void setEndpoint(String endpoint) {
+		ENDPOINT = endpoint;
+		System.out.println("Endpoint is: " + ENDPOINT);
 	}
-	
-	public static RequestSpecification getRequestSpecification()
-	{
-		REQ_SPEC_BUILDER=new RequestSpecBuilder();
-		System.out.println("Base URI: "+Base.config.getString("baseURI"));
+
+	public static RequestSpecification getRequestSpecification() {
+		REQ_SPEC_BUILDER = new RequestSpecBuilder();
+		System.out.println("Base URI: " + Base.config.getString("baseURI"));
 		REQ_SPEC_BUILDER.setBaseUri(Base.config.getString("baseURI"));
 		REQ_SPEC_BUILDER.setBasePath(Base.config.getString("basePath"));
 		REQ_SPEC_BUILDER.setAccept(Base.config.getString("Accept"));
@@ -43,114 +43,113 @@ public class RestUtilities {
 		REQ_SPECIFICATION = REQ_SPEC_BUILDER.build();
 		return REQ_SPECIFICATION;
 	}
-	
-	public static void setRequestHeader(HashMap<String,String> hm, RequestSpecification spec)
+
+	public static ResponseSpecification getResponseSpecification()
 	{
-		REQ_SPECIFICATION=REQ_SPECIFICATION.spec(spec);
+		RES_SPEC_BUILDER = new ResponseSpecBuilder();
+		RES_SPEC_BUILDER.expectStatusCode(200);
+		RES_SPEC_BUILDER.expectResponseTime(lessThan(5000L), TimeUnit.MILLISECONDS);
+		RES_SPEC_BUILDER.expectStatusLine(containsStringIgnoringCase("OK"));
+		RES_SPECIFICATION=RES_SPEC_BUILDER.build();
+		return RES_SPECIFICATION;
+	}
+	
+public static void setRequestHeader(HashMap<String, String> hm, RequestSpecification spec) {
+		REQ_SPECIFICATION = REQ_SPECIFICATION.spec(spec);
 		REQ_SPECIFICATION.headers(hm);
-		System.out.println("Request Headers "+REQ_SPECIFICATION.log().headers());
+		System.out.println("Request Headers " + REQ_SPECIFICATION.log().headers());
 	}
 
-	public static void setQueryparams(HashMap<String,String> hm,RequestSpecification spec)
-	{
-		REQ_SPECIFICATION=REQ_SPECIFICATION.spec(spec);
+	public static void setQueryparams(HashMap<String, String> hm, RequestSpecification spec) {
+		REQ_SPECIFICATION = REQ_SPECIFICATION.spec(spec);
 		REQ_SPECIFICATION.queryParams(hm);
 	}
-	
-	public static void setQueryparam(String key, String value,RequestSpecification spec)
-	{
-		REQ_SPECIFICATION=REQ_SPECIFICATION.spec(spec);
-		REQ_SPECIFICATION.queryParam(key,value);
+
+	public static void setQueryparam(String key, String value, RequestSpecification spec) {
+		REQ_SPECIFICATION = REQ_SPECIFICATION.spec(spec);
+		REQ_SPECIFICATION.queryParam(key, value);
 	}
-	
-	public static void setQueryparams(String key, ArrayList<String> values,RequestSpecification spec)
-	{
-		REQ_SPECIFICATION=REQ_SPECIFICATION.spec(spec);
+
+	public static void setQueryparams(String key, ArrayList<String> values, RequestSpecification spec) {
+		REQ_SPECIFICATION = REQ_SPECIFICATION.spec(spec);
 		REQ_SPECIFICATION.queryParam(key, values);
 	}
-	
-	public static void setPathParameter(String key, String value, RequestSpecification spec)
-	{
-		REQ_SPECIFICATION=REQ_SPECIFICATION.spec(spec);
+
+	public static void setPathParameter(String key, String value, RequestSpecification spec) {
+		REQ_SPECIFICATION = REQ_SPECIFICATION.spec(spec);
 		REQ_SPECIFICATION.pathParam(key, value);
 	}
-	
-	public static void setPathParameter(String key, ArrayList<String> values, RequestSpecification spec)
-	{
-		REQ_SPECIFICATION=REQ_SPECIFICATION.spec(spec);
+
+	public static void setPathParameter(String key, ArrayList<String> values, RequestSpecification spec) {
+		REQ_SPECIFICATION = REQ_SPECIFICATION.spec(spec);
 		REQ_SPECIFICATION.pathParam(key, values);
 	}
-	
-	public static void setPathParameter(String key, HashMap<String,String> values, RequestSpecification spec)
-	{
-		REQ_SPECIFICATION=REQ_SPECIFICATION.spec(spec);
+
+	public static void setPathParameter(String key, HashMap<String, String> values, RequestSpecification spec) {
+		REQ_SPECIFICATION = REQ_SPECIFICATION.spec(spec);
 		REQ_SPECIFICATION.pathParams(values);
 	}
 
-	public static String getConfigValue(String key)
-	{
-			return Base.config.getString(key);
+	public static String getConfigValue(String key) {
+		return Base.config.getString(key);
 	}
-	
-	public static Response getResponse()
-	{
+
+	public static Response getResponse() {
 		Response response = given().spec(REQ_SPECIFICATION).when().get(ENDPOINT);
 		return response;
 	}
-	
-	public static Response getResponse(MethodType methodType, String payload)
-	{
+
+	public static Response getResponse(MethodType methodType, String payload) {
 		Response response = null;
-		switch(methodType)
-		{
+		switch (methodType) {
 		case POST:
 			response = given().spec(REQ_SPECIFICATION).log().all(true).body(payload).when().post(ENDPOINT);
 			System.out.println("Response body is: ");
 			System.out.print(response.body().asString());
-		break;
-		
-		case GET: response = given().spec(REQ_SPECIFICATION).log().all(true).when().get(ENDPOINT);
-		break;
-		
-		case PUT: response = given().spec(REQ_SPECIFICATION).when().put(ENDPOINT);
-		break;
-		
-		case DELETE: response = given().spec(REQ_SPECIFICATION).when().delete(ENDPOINT);
-		break;
-		
-		default: System.out.println("Method Type is not proper.");
-		break;
-		
+			break;
+
+		case GET:
+			response = given().spec(REQ_SPECIFICATION).log().all(true).when().get(ENDPOINT);
+			System.out.println("Response body is: ");
+			System.out.print(response.body().asString());
+			break;
+
+		case PUT:
+			response = given().spec(REQ_SPECIFICATION).when().put(ENDPOINT);
+			break;
+
+		case DELETE:
+			response = given().spec(REQ_SPECIFICATION).when().delete(ENDPOINT);
+			break;
+
+		default:
+			System.out.println("Method Type is not proper.");
+			break;
+
 		}
 		return response;
 	}
 
-	public static XmlPath getXmlPath(Response response)
-	{
+	public static XmlPath getXmlPath(Response response) {
 		return response.xmlPath();
 	}
 
-	public static JsonPath getJsonPath(Response response)
-	{
+	public static JsonPath getJsonPath(Response response) {
 		return response.jsonPath();
 	}
 
-	public static String getStringValuefromXMLResponse(XmlPath xmlPath, String path)
-	{
+	public static String getStringValuefromXMLResponse(XmlPath xmlPath, String path) {
 		return xmlPath.get(path).toString();
 	}
-	
-	public static int getIntValuefromXMLResponse(XmlPath xmlPath, String path)
-	{
+
+	public static int getIntValuefromXMLResponse(XmlPath xmlPath, String path) {
 		return xmlPath.getInt(path);
 	}
 
-	public static void setConfigValue(String key, String value) throws ConfigurationException
-	{
+	public static void setConfigValue(String key, String value) throws ConfigurationException {
 		Base.config.setDelimiterParsingDisabled(true);
 		Base.config.setProperty(key, value);
 		Base.config.save();
 	}
-
 
 }
